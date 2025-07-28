7765f361bbbc859f01acf997145e5523a16e68e1
littlesekii <contact.sekii@gmail.com>
2025-07-27 21:58:35 -0300

    feat.: updated LoginView and RegisterView with components

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,31 @@
 <script setup>
-
+import LoginFormComponent from "@/components/AuthFormComponent/LoginFormComponent.vue";
 </script>
 
 <template>
-  <div>
-
-  </div>
+  <main class="main-container flex">
+    <div class="left-container flex f-centered">
+      <LoginFormComponent/>
+    </div>
+    <aside class="right-container">
+      
+    </aside>
+  </main>
 </template>
 
-
-
 <style scoped>
+.main-container {
+  min-height: 100dvh;
+}
 
+.left-container {
+  flex: 1;
+}
+.right-container {
+  flex: 1;
+
+  background-image: url("https://assets.production.linktr.ee/auth/3448/media/banner-login-desktop.f355be949b508c58ec2d.webp");
+  background-size: cover;
+  background-position: center;
+}
 </style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,11 +1,32 @@
 <script setup>
+import RegisterFormComponent from "@/components/AuthFormComponent/RegisterFormComponent/RegisterFormComponent.vue";
 
 </script>
 
 <template>
-  
+  <main class="main-container flex">
+    <div class="left-container flex f-centered">
+      <RegisterFormComponent/>
+    </div>
+    <aside class="right-container">
+      
+    </aside>
+  </main>
 </template>
 
 <style scoped>
+.main-container {
+  min-height: 100dvh;
+}
 
+.left-container {
+  flex: 1;
+}
+.right-container {
+  flex: 1;
+
+  background-image: url("https://assets.production.linktr.ee/auth/3448/media/banner-login-desktop.f355be949b508c58ec2d.webp");
+  background-size: cover;
+  background-position: center;
+}
 </style>
