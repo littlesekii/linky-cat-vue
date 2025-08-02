f83454b9fb7a6d74a4d6ef75141545b76c7f1feb
littlesekii <contact.sekii@gmail.com>
2025-08-01 21:09:31 -0300

    feat.: changed RegisterFormComponent to work as a "wizard" with separated steps

--- src/components/AuthFormComponent/RegisterFormComponent/RegisterFormComponent.vue
+++ src/components/AuthFormComponent/RegisterFormComponent/RegisterFormComponent.vue
@@ -1,97 +1,71 @@
 <script setup>
+import { reactive, ref } from "vue";
+import RegisterFormEmailComponent from "./steps/step1/RegisterFormEmailComponent.vue";
+import RegisterFormUserNameComponent from "./steps/step2/RegisterFormUserNameComponent.vue";
+import RegisterFormEmailVerificationComponent from "./steps/step3/RegisterFormEmailVerificationComponent.vue";
+import RegisterFormPasswordComponent from "./steps/step4/RegisterFormPasswordComponent.vue";
+import RegisterFormPageNameComponent from "./steps/step5/RegisterFormPageNameComponent.vue";
+
+const registerData = reactive({
+	username: "",
+	email: "",
+	password: "",
+	firstName: "",
+	lastName: "",
+});
+
+const currentStep = ref(0);
+const registrationSteps = [
+	"email", 
+	"username", 
+	"email-verification",
+	"password",
+	"page-name"
+];
+
+function continueRegister(data) {
+	if (registrationSteps[currentStep.value] == "email") {
+		registerData.email = data.email;
+	}
+	if (registrationSteps[currentStep.value] == "username") {
+		registerData.username = data.username;
+	}
+
+	++currentStep.value;
+}
 
 </script>
 
 <template>
-	<section class="register-container flex f-column">
-		<header class="header flex f-column">
-			<h1 class="title">Join Linky Cat</h1>
-			<p class="text">Create your account for free!</p>
-		</header>
+	<!-- <div class="flex f-column f-centered"> -->
+		<RegisterFormEmailComponent 
+			v-if="registrationSteps[currentStep] == 'email'" 
+			@continue="continueRegister"
+		/>
+		<RegisterFormUserNameComponent 
+			v-if="registrationSteps[currentStep] == 'username'" 
+			@continue="continueRegister"
+		/>
+		<RegisterFormPasswordComponent
+			v-if="registrationSteps[currentStep] == 'password'" 
+			@continue="continueRegister"
+		/>
+		<RegisterFormEmailVerificationComponent
+			v-if="registrationSteps[currentStep] == 'email-verification'" 
+			@continue="continueRegister"
+		/>
+		<RegisterFormPageNameComponent 
+			v-if="registrationSteps[currentStep] == 'page-name'" 
+			@continue="continueRegister"
+		/>
+		<!-- <br>
+		{{ registerData }}
+	</div> -->
 
-		<form class="form flex f-column">
-			<input class="input" type="email" placeholder="Email">
-			<button class="button b-inctive">Continue</button>
-			<p class="text">Already have an account? <a class="link" href="/login">Log in</a></p>
-		</form>
-
-	</section>
+	
 </template>
 
 
 <style scoped>
-.text {
-	color: var(--text-color-light);
-	font-weight: lighter;
-}
 
-.register-container {
-	width: 90%;
-
-	.header {
-		margin-bottom: 30px;
-		text-align: center;
-
-		.title {
-			margin-bottom: 15px;
-			font-size: 34pt;
-			font-weight: bold;
-		}
-	}
-
-	.form {
-		margin: auto;
-		width: 100%;
-		max-width: 500px;
-
-		text-align: center;
-
-		.input, .button {
-			margin-bottom: 10px;
-
-			padding: 12px 15px;	
-			font-size: 12pt;
-		}
-
-		.input {
-			border: none;
-			/* border: 1.5px solid var(--text-color-light); */
-			border-radius: 10px;
-			font-size: 11pt;
-
-			background-color: var(--color-gray-lighter);
-			color: var(--color-black);
-		}
-
-		.input::placeholder {
-			color: var(--color-gray-dark);
-		}
-
-		.button {
-      /* margin-top: 15px; */
-			border: none;
-			border-radius: 25px;
-
-			background-color: var(--button-color);
-			color: var(--color-white);
-			
-			font-weight: bold;
-
-			cursor: pointer;
-		}
-
-		.b-inactive {
-			background-color: var(--color-gray-light);
-			color: var(--color-black-light);
-		}
-
-		.text {
-			font-size: 11pt;
-
-			.link {
-				color: inherit;
-			}
-		}
-	}
-}
 </style>
---
+++ src/components/AuthFormComponent/RegisterFormComponent/steps/step2/RegisterFormUserNameComponent.vue
@@ -0,0 +1,168 @@
+
+<script setup>
+import api from "@/api/api";
+import AuthInputComponent from "@/components/AuthFormComponent/AuthInputComponent.vue";
+import utils from "@/utils/utils";
+import { ref, useTemplateRef } from "vue";
+
+const username = ref("");
+const isLoading = ref(false);
+const canContinue = ref(false);
+const internalErrorMsg = ref("");
+
+const inputRef = useTemplateRef("input");
+const emit = defineEmits(["continue"]);
+
+const debouncedValidate = utils.debounce(validate, 700);
+
+function showInternalError(msg) {
+  internalErrorMsg.value = msg;
+}
+
+function removeInternalError() {
+  internalErrorMsg.value = "";
+}
+
+async function validate(username) {
+	canContinue.value = false;
+
+	if (!inputRef.value.validate())
+		return false;
+
+	
+
+  // Server validation
+  try {
+    isLoading.value = true;
+    removeInternalError();
+
+		const body = {
+			"username": username,
+		};
+
+    const res = await api.async.post("/api/auth/register/check-username", JSON.stringify(body));
+    isLoading.value = false;
+		
+    if (!res.ok) {
+      const data = await res.json();
+      if (data.errorCode === "AUTH003") {
+        inputRef.value.showError("This username is already taken");
+        return false;
+      }
+			
+      showInternalError("A unexpected error occurred");
+      return false;
+    }
+  } catch {
+    isLoading.value = false;
+    showInternalError("A internal error occurred, please try again later");
+    return false;
+  }
+
+	canContinue.value = true;
+	return true;
+}
+
+async function continueRegister() {
+		
+  if (!canContinue.value)
+		return;
+
+	if (!await validate(username.value))
+		return;
+
+	emit("continue", { username: username.value } );
+}
+
+</script>
+
+<template>
+  <section class="register-container flex f-column">
+		<header class="header flex f-column">
+			<h1 class="title">Join Linky Cat</h1>
+			<p class="text">Create your account for free!</p>
+		</header>
+
+		<p 
+			class="text internal-error-text" 
+			v-if=" internalErrorMsg !== '' "
+		>
+			{{ internalErrorMsg }}
+		</p>
+    <form class="form flex f-column" @submit.prevent="continueRegister">
+      <AuthInputComponent 
+        type="text" 
+        v-model="username" 
+        placeholder="linky.cat/" 
+				ph-fixed="true"
+        ref="input" 
+
+        @input="debouncedValidate(username)"
+      />
+      <AuthInputComponent
+        type="button" 
+        :disabled="!canContinue"
+        button-text="Continue" 
+      />
+      <p class="text">
+        Already have an account? <RouterLink class="link" to="/login">Log in</RouterLink>
+      </p>
+    </form>
+  </section>
+</template>
+
+<style scoped>
+.text {
+	color: var(--text-light-color);
+	font-weight: lighter;
+}
+
+.register-container {
+	width: 90%;
+
+	.header {
+		margin-bottom: 30px;
+		text-align: center;
+
+		.title {
+			margin-bottom: 15px;
+			font-size: 34pt;
+			font-weight: bold;
+		}
+	}
+
+	.internal-error-text {
+		padding-bottom: 4px;
+		padding-left: 2px;
+		text-align: center;
+		font-size: 11pt;
+		font-weight: 500;
+		color:var(--error-color);
+	}
+
+	.form {
+		margin: auto;
+		width: 100%;
+		max-width: 500px;
+
+		text-align: center;
+
+		.text {
+			font-size: 11pt;
+			.link {
+				color: inherit;
+			}
+		}
+
+		.input-error-text {
+			padding-bottom: 2px;
+			padding-left: 2px;
+			text-align: left;
+			font-size: 11pt;
+			font-weight: 500;
+			color:var(--error-color);
+		}
+	}
+}
+
+</style>
